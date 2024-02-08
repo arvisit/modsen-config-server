1036008b190a48f735d4fcdb70f61fcfa7cd7b15
Artem Sitko <sitko.ar@gmail.com>
2024-02-08 22:42:37 +0300

    Enable discovery client

--- src/main/java/by/arvisit/modsenlibapp/configserver/ModsenConfigServerApplication.java
+++ src/main/java/by/arvisit/modsenlibapp/configserver/ModsenConfigServerApplication.java
@@ -2,10 +2,12 @@ package by.arvisit.modsenlibapp.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ModsenConfigServerApplication {
 
     public static void main(String[] args) {
